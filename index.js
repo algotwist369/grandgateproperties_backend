23ef5502a0e538d6cf12bb03f93fbda447e25a14
ankit pathak <info.algotwist@gmail.com>
2026-02-10 12:38:07 +0530

    update express body upto 50mb data

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ if (cluster.isMaster) {
     app.use(cookieParser());
 
     // Body parser
-    app.use(express.json({ limit: '10kb' })); // Limit body size
+    app.use(express.json({ limit: '50mb' }));  
+    app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
     // Sanitize data (NoSQL Injection)
     app.use(mongoSanitize());
